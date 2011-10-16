bcc80541af853bc034fadae7a61006ab84b5bdf3
Tomek <t.mlodawski@gmail.com>
2011-10-16 13:43:52 +0200

    dziala

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -1,20 +1,26 @@
 class MenuController < ApplicationController
   def home
+@title="Home"
   end
 
   def firma
+	@title="Firma"
   end
 
   def klienci
+@title="Klienci"
   end
 
   def kontakt
+@title="Kontakt"
   end
 
   def oferta
+	@title="Oferta"
   end
 
   def portfolio
+@title="Portfolio"  
   end
 
 end
